765d8918e073c942655dfe6d941d282c5cbdc9d2
vincentvermillion <vvermi2@wgu.edu>
2023-12-17 18:41:46 -0600

    created a front page edited pom.xlm to include spring-boot-devtools

--- src/main/java/com/example/mycoolap/rest/FrontPage.java
+++ src/main/java/com/example/mycoolap/rest/FrontPage.java
@@ -1,10 +1,34 @@
 package com.example.mycoolap.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class FrontPage {
+
     @GetMapping("/")
     public String frontPage(){
         return "Extra ecclesiam nulla salus";
     }
+
+    // expose a new endpoint for workout
+
+    @GetMapping("/workout")
+    public String anotherMessage(){
+        return "I'm awesome";
+    }
+
+    // expose another endpoint
+
+    @GetMapping("/prayer")
+    public String ourFather(){
+        return "Our Father, who art in heaven";
+    }
+
+    @GetMapping("/spouse")
+    public String spouse(){
+        return "Nicole Vermillion";
+    }
+
 }
+
