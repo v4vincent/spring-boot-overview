b5d34d7f88f7013a6a5fb33ee2af6fec0c647cad
vincentvermillion <vvermi2@wgu.edu>
2023-12-18 22:09:11 -0600

    Used application.properties to create proterties to inject into the rest controller.

--- src/main/java/com/example/mycoolap/rest/FrontPage.java
+++ src/main/java/com/example/mycoolap/rest/FrontPage.java
@@ -1,10 +1,31 @@
 package com.example.mycoolap.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FrontPage {
+
+    //inject properties from application.properties
+    //spouse.name and spouse.age
+    @Value("${spouse.name}")
+    private String spouseName;
+
+    @Value("${spouse.age}")
+    private int spouseAge;
+
+    // expose a new endpoint for spouse info
+
+    @GetMapping("/spouseinfo")
+    public String getSpouseName(){
+        return "My wife is " + spouseName + " and she is " + spouseAge + " years old.";
+    }
+
+
+
+
+
 
     @GetMapping("/")
     public String frontPage(){
@@ -29,6 +50,10 @@ public class FrontPage {
     public String spouse(){
         return "Nicole Vermillion";
     }
+
+
+
+
 
 }
 
